7c7e7d1b6cc4cfced0665ed37a29beb48e5535b2
zembaw05 <alibarj1999@hotmail.com>
2021-12-03 23:13:34 +0100

    modifications in searchbar

--- frontend/searchbar/searchbar.js
+++ frontend/searchbar/searchbar.js
@@ -2,49 +2,60 @@
 
 
 const downloadLink = document.getElementById('downloadButton');
-  const stopButton = document.getElementById('stopButton');
-  const startButton = document.getElementById('startButton');
+const stopButton = document.getElementById('stopButton');
+const startButton = document.getElementById('startButton');
 
-  const handleSuccess = function(stream) {
-    const options = {mimeType: 'audio/webm'};
-    const recordedChunks = [];
-    const mediaRecorder = new MediaRecorder(stream, options);
+const handleSuccess = function(stream) {
+  const options = {mimeType: 'audio/webm'};
+  const recordedChunks = [];
+  const mediaRecorder = new MediaRecorder(stream, options);
 
-    mediaRecorder.addEventListener('dataavailable', function(e) {
-      console.log("what?")
-      if (e.data.size > 0) recordedChunks.push(e.data);
-    });
+  mediaRecorder.addEventListener('dataavailable', function(e) {
+    // console.log("what?")
+    if (e.data.size > 0) recordedChunks.push(e.data);
+  });
 
-    mediaRecorder.addEventListener('stop', function() {
-      console.log('stopped recording?')
-      downloadLink.href = URL.createObjectURL(new Blob(recordedChunks));
-      let data = new FormData();
-      data.append('sound', new Blob(recordedChunks));
-      data.append('test', 'test');
-      console.log(data.getAll('test'))
+  mediaRecorder.addEventListener('stop', function() {
+    console.log('sending recorded sound')
+    let data = new FormData();
+    let blob =  new Blob(recordedChunks, {'type': 'audio/wav;'})
+    // downloadLink.href = URL.createObjectURL(blob);
+    // downloadLink.download = 'blob.wav';
+    let reader = new FileReader();
+    reader.readAsDataURL(blob);
+    reader.onloadend = function () {
+    let base64String = reader.result;
+    
+    data.append('sound', blob);
+    console.log(blob);}
+    // console.log(data.getAll('test'))
 
 
 
 
-      let ajaxRequest = new XMLHttpRequest();
-      ajaxRequest.onreadystatechange = function(){
-        if(ajaxRequest.readyState == 4){
-          if (ajaxRequest.status == 200){
-            console.log(ajaxRequest.responseText)
-          }
-        else if(ajaxRequest.status == 0){
-          alert("Aucune réponse du serveur");
+
+
+    let ajaxRequest = new XMLHttpRequest();
+    ajaxRequest.onreadystatechange = function(){
+      if(ajaxRequest.readyState == 4){
+        if (ajaxRequest.status == 200){
+          console.log(ajaxRequest.responseText)
         }
-        else{
-          alert(ajaxRequest.responseText);
-
-        }
-      
-        }
+      else if(ajaxRequest.status == 0){
+        alert("Aucune réponse du serveur");
       }
-      ajaxRequest.open('POST', "http://localhost:5001/client");
-      // ajaxRequest.setRequestHeader("Content-type", "application/json");
-      ajaxRequest.send(data);
+      else{
+        alert(ajaxRequest.responseText);
+
+      }
+    
+      }
+    }
+    ajaxRequest.open('POST', "http://localhost:5000/get_the_voice");
+    // ajaxRequest.setRequestHeader("Content-type", "form-data");
+
+    ajaxRequest.send(data);
+    console.log('recorded data sent to server');
 
 
 
@@ -60,18 +71,20 @@ const downloadLink = document.getElementById('downloadButton');
 
 
 
-      //downloadLink.download = 'sound1.wav';
-    });
+    //downloadLink.download = 'sound1.wav';
+  });
 
-    stopButton.addEventListener('click', function() {
-      mediaRecorder.stop();
-    });
+  stopButton.addEventListener('click', function() {
+    mediaRecorder.stop();
+    console.log("recording stoped");
+  });
 
-    startButton.addEventListener('click', function() {
-      mediaRecorder.start();
-    });
+  startButton.addEventListener('click', function() {
+    mediaRecorder.start();
+    console.log("recording started");
+  });
 
-  };
+};
 
-  navigator.mediaDevices.getUserMedia({ audio: true, video: false })
-      .then(handleSuccess);
+navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+    .then(handleSuccess);
